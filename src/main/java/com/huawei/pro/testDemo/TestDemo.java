1d1940850d5c28822bf2bb6f63c829d4fe307020
jeffChen_move <1842428845@qq.com>
2018-12-03 21:43:48 +0800

    Excel绘制工具类默认样式添加

--- src/main/java/com/huawei/pro/testDemo/TestDemo.java
+++ src/main/java/com/huawei/pro/testDemo/TestDemo.java
@@ -6,6 +6,9 @@ import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
@@ -48,19 +51,19 @@ public class TestDemo {
             // 生成一个样式
             HSSFCellStyle style = workbook.createCellStyle();
             // 设置这些样式
-            style.setFillForegroundColor(HSSFColor.PALE_BLUE.index);
-            style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-            style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-            style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-            style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-            style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-            style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+            style.setFillForegroundColor(HSSFColor.HSSFColorPredefined.PALE_BLUE.getIndex());
+            style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            style.setBorderBottom(BorderStyle.THIN);
+            style.setBorderLeft(BorderStyle.THIN);
+            style.setBorderTop(BorderStyle.THIN);
+            style.setBorderRight(BorderStyle.THIN);
+            style.setAlignment(HorizontalAlignment.CENTER);
 
             // 生成一个字体
             HSSFFont font = workbook.createFont();
-            font.setColor(HSSFColor.BLACK.index);
+            font.setColor(HSSFColor.HSSFColorPredefined.BLACK.getIndex());
             font.setFontHeightInPoints((short) 12);
-            font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+            font.setBold(true);
 
             // 把字体应用到当前的样式
             style.setFont(font);
